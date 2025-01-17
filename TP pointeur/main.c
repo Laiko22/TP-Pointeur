b760aa27d4f34c6f91522380cdb4cfab65268c85
adril <adril@MSI>
2025-01-17 14:36:21 +0100

    Paramètre de Fonction

--- TP pointeur/main.c
+++ TP pointeur/main.c
@@ -1,14 +1,42 @@
+// Adrien Gillet
+
+
 #include <stdio.h>
 
-int main() {
 
-	int a = 2;
-	printf("%p", &a);
-	int *p = &a;
-	printf("\n%p", p);
-	printf("\n%p", &p);
-	*p = 5;
-	printf("\n%d", a);
+void min_max(int tab[], int size, int* min, int* max)
+{
+	if (size < 1)
+	{
+		return;
+	}
 
+	*min = tab[0];
+	*max = tab[0];
+
+	for (int i = 0; i < size; i++)
+	{
+		if (*min > tab[i])
+		{
+			*min = tab[i];
+
+		}
+
+		if (*max < tab[i])
+		{
+			*max = tab[i];
+
+		}
+
+	}
+
+}
+int main()
+{
+	int min = 0;
+	int max = 0;
+	int tableau[200] = {12, 2 , 8 , -7 , 15 , 2};
+	min_max(tableau, 6, &min, &max);
+	printf(" min = % d, max = % d\n ", min, max);
 	return 0;
 }
