8a46d006c3a3b4339a1076d11a06667ff2cd1153
adril <adril@MSI>
2025-01-18 22:38:04 +0100

    Etape 4 : Ajouter le menu principal

--- TP pointeur/main.c
+++ TP pointeur/main.c
@@ -15,6 +15,7 @@ typedef struct Application {
 
 	}Application;
 
+
 void runApplication(const Application* app) {
 
 	int j, k;
@@ -29,27 +30,55 @@ void runApplication(const Application* app) {
 		printf("\nOption invalide !");
 	}
 
-	printf("\n\nPour quitter, appuyez sur 0.\n");
+	printf("\n\nPour quitter, appuyez sur 0 : ");
 	scanf_s("%d", &k);
+
 	if (k == 0)
-		printf("Fermeture de l'application");
+		printf("\n\n---Fermeture de l'application---\n");
 }
 
 void displayMenu(const Application *app) {
 
-	printf("--%s--\n",app->nom);
+	printf("\n--%s--\n",app->nom);
 
 	for (int i = 0; i < app->nb_menus; i++) {
 
 		printf("%d.%s\n",i+1,app->menus[i].option);
-
 	}
 
 	runApplication(app);
 }
 
+void runPhone(Application* app1, Application* app2, Application* app3) {
+
+
+	int l;
+
+	printf("---Applications---");
+	printf("\n1. %s", app1->nom);
+	printf("\n2. %s", app2->nom);
+	printf("\n3. %s", app3->nom);
+	printf("\n0. Quittez\n");
+
+	printf("\nVeuillez choisir une application : ");
+	scanf_s("%d", &l);
+
+	if (l == 1) {
+		displayMenu(app1);
+	}
+	if (l == 2) {
+		displayMenu(app2);
+	}
+	if (l == 3) {
+		displayMenu(app3);
+	}
+	else
+		;
+}
+
 int main()
 {
+
 	Menu photoMenus[] = {"Regarder une photo","Modifier une photo" };
 
 	Application Photo;
@@ -58,16 +87,30 @@ int main()
 	Photo.menus = photoMenus;
 
 
-	displayMenu(&Photo);
+	
+	Menu messageMenus[] = { "Lire un message","Envoyer un message" };
 
+	Application Message;
+	Message.nom = "Message";
+	Message.nb_menus = 2;
+	Message.menus = messageMenus;
+
+
+
+	Menu youtubeMenus[] = { "Lire une video","S'abonner a une chaine" };
 
 	Application Youtube;
-
 	Youtube.nom = "Youtube";
-	Youtube.nb_menus = 1;
+	Youtube.nb_menus = 2;
+	Youtube.menus = youtubeMenus;
 
 	
 
+
+	
+	runPhone(&Youtube, &Photo, &Message);
+	
+	
 
 	return 0;
 }
